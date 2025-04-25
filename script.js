1522279fb1f713aba2ea852a06da3b09596f53a2
Roenne <roennearjona07@gmail.com>
2025-04-25 10:51:15 +0000

    ang landi

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("Babbyyyyyy")
+alert("Pansinin mo koo!!!!! tamaa na larooo!")
+alert("puro ka drugs ")
+alert("ako namaaaannnn")
+alert("I misss youuu babyyy kooo")
+alert("mine ka munaa tuloy mo nalang yan tomorrow")
+alert("waabyuuuu ")
